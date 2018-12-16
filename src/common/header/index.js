2bf9ead5578ac56a90dd5ca8241d637f9f319c55
juqiangh <18296154764@163.com>
2018-12-16 13:56:24 +0800

    header开发

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { CSSTransition } from 'react-transition-group';
 import { actionCreators } from './store';
@@ -8,64 +8,138 @@ import {
     Nav,
     NavItem,
     NavSearch,
+    SearchInfo,
+    SearchInfoTitle,
+    SearchInfoSwitch,
+    SearchInfoItem,
+    SearchInfoList,
     Addition,
     Button,
     SearchWrapper
 } from './style';
 
-const Header = (props) => {
-    return (
-        <HeaderWrapper>
-            <Logo/>
-            <Nav>
-                <NavItem className='left'>首页</NavItem>
-                <NavItem className='left'>下载App</NavItem>
-                <NavItem className='right'>登录</NavItem>
-                <NavItem className='right'>
-                    <i className="iconfont">&#xe636;</i>
-                </NavItem>
-                <SearchWrapper>
-                    <CSSTransition
-                        in={props.focused}
-                        timeout={200}
-                        classNames="slide"
-                    >
-                        <NavSearch
-                            className={props.focused ? 'focused' : ''}
-                            onFocus={props.handleInputFocus}
-                            onBlur={props.handleInputBlur}
-                        ></NavSearch>
-                    </CSSTransition>
-                    <i className={props.focused ? 'focused iconfont' : 'iconfont'}
-                    >
-                        &#xe64d;
-                    </i>
-                </SearchWrapper>
-            </Nav>
-            <Addition>
-                <Button className='writting'>
-                    <i className="iconfont">&#xe624;</i>
-                    写文章
-                </Button>
-                <Button className='reg'>注册</Button>
-            </Addition>
-        </HeaderWrapper>
-    )
+class Header extends Component {
+
+    getListArea() {
+        const { focused, list, page, mouseIn, totalPage,  handleMouseEnter, handleMouseLeave, handleChangePage} = this.props;
+        const newList = list.toJS();
+        const pageList = [];
+        if(newList.length) {
+            for(let i = ( page - 1 ) * 10; i < page * 10; i++ ) {
+                pageList.push(
+                    <SearchInfoItem key={newList[i]}>{newList[i]}</SearchInfoItem>
+                )
+            }
+        }
+        if(focused || mouseIn) {
+            return (
+                <SearchInfo
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
+                >
+                    <SearchInfoTitle>
+                        热门搜索
+                        <SearchInfoSwitch
+                            onClick={() => {
+                                handleChangePage(page, totalPage, this.spinIcon)
+                            }}
+                        >
+                            <i ref={(icon) => {this.spinIcon = icon}} className='iconfont spin'>
+                                &#xe851;
+                            </i>
+                            换一批
+                        </SearchInfoSwitch>
+                    </SearchInfoTitle>
+                    <SearchInfoList>
+                        {pageList}
+                    </SearchInfoList>
+                </SearchInfo>
+            )
+        }else {
+            return null;
+        }
+    }
+
+    render() {
+        const { focused, list, handleInputFocus, handleInputBlur } = this.props;
+        return (
+            <HeaderWrapper>
+                <Logo/>
+                <Nav>
+                    <NavItem className='left'>首页</NavItem>
+                    <NavItem className='left'>下载App</NavItem>
+                    <NavItem className='right'>登录</NavItem>
+                    <NavItem className='right'>
+                        <i className="iconfont">&#xe636;</i>
+                    </NavItem>
+                    <SearchWrapper>
+                        <CSSTransition
+                            in={focused}
+                            timeout={200}
+                            classNames="slide"
+                        >
+                            <NavSearch
+                                className={focused ? 'focused' : ''}
+                                onFocus={() => handleInputFocus(list)}
+                                onBlur={handleInputBlur}
+                            ></NavSearch>
+                        </CSSTransition>
+                        <i className={focused ? 'focused iconfont zoom' : 'iconfont zoom'}>
+                            &#xe64d;
+                        </i>
+                        {this.getListArea()}
+                    </SearchWrapper>
+                </Nav>
+                <Addition>
+                    <Button className='writting'>
+                        <i className="iconfont">&#xe624;</i>
+                        写文章
+                    </Button>
+                    <Button className='reg'>注册</Button>
+                </Addition>
+            </HeaderWrapper>
+        )
+    }
 }
 
 const mapStateToProps = (state) => {
     return {
-        focused: state.getIn(['header','focused'])
+        focused: state.getIn(['header','focused']),
+        list: state.getIn(['header', 'list']),
+        page: state.getIn(['header', 'page']),
+        mouseIn: state.getIn(['header', 'mouseIn']),
+        totalPage: state.getIn(['header', 'totalPage'])
     }
 }
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        handleInputFocus() {
+        handleInputFocus(list) {
+            (list.size===0) && dispatch(actionCreators.getList());
             dispatch(actionCreators.searchFocus());
         },
         handleInputBlur() {
             dispatch(actionCreators.searchBlur());
+        },
+        handleMouseEnter() {
+            dispatch(actionCreators.mouseEnter());
+        },
+        handleMouseLeave() {
+            dispatch(actionCreators.mouseLeave());
+        },
+        handleChangePage(page, totalPage, spin) {
+            let originAngle = spin.style.transform.replace(/[^0-9]/ig, '');
+            if(originAngle) {
+                originAngle = parseInt(originAngle, 10);
+            }else {
+                originAngle = 0;
+            }
+            spin.style.transform = 'rotate(' + (originAngle + 360) + 'deg)';
+            if(page < totalPage) {
+                dispatch(actionCreators.changePage(page + 1));
+            }else{
+                dispatch(actionCreators.changePage(1));
+            }
         }
     }
 }
--- src/common/header/style.js
+++ src/common/header/style.js
@@ -44,7 +44,7 @@ export const NavItem = styled.div`
 export const SearchWrapper = styled.div`
     position: relative;
     float: left;
-    .iconfont {
+    .zoom {
         position: absolute;
         right: 0px;
         width: 38px;
@@ -92,6 +92,49 @@ export const NavSearch = styled.input.attrs({
     &.slide-exit-active {
         width: 180px;
     }
+`;
+export const SearchInfo = styled.div`
+    position: absolute;
+    left: 0;
+    top: 56px;
+    width: 240px;
+    padding: 0 20px;
+    box-shadow: 0 0 8px rgba(0, 0, 0, .2);
+`;
+export const SearchInfoTitle = styled.div`
+    margin-top: 20px; 
+    margin-bottom: 15px;
+    line-height: 20px;
+    font-size: 14px;
+    color: #969696;
+`;
+export const SearchInfoSwitch = styled.span`
+    float: right;
+    font-size: 13px;
+    cursor: pointer;
+    .spin {
+		display: block;
+		float: left;
+		font-size: 12px;
+		margin-right: 2px;
+		transition: all .2s ease-in;
+		transform-origin: center center;
+	}
+`;
+export const SearchInfoList = styled.div`
+    overflow: hidden;
+`;
+export const SearchInfoItem = styled.a`
+    display: block;
+    float: left;
+    padding: 0 5px;
+    margin-right: 10px;
+    margin-bottom: 15px;
+    line-height: 20px;
+    font-size: 12px;
+    border: 1px solid #ddd;
+    color: #787878;
+    border-radius: 3px;
 `;
 export const Addition = styled.div`
     position: absolute;
